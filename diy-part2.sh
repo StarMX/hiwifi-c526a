d64f3b7b2db64ecf7a4226b8c9a31c071be9af90
StarMX <5440909@qq.com>
2022-06-01 16:29:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,13 +13,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.1.2.1/g' package/base-files/files/bin/config_generate
 
-sed -i 's/CONFIG_PACKAGE_luci-app-ipsec-vpnd/#CONFIG_PACKAGE_luci-app-ipsec-vpnd/g' .config
-sed -i 's/CONFIG_PACKAGE_luci-app-unblockneteasemusic/#CONFIG_PACKAGE_luci-app-unblockneteasemusic/g' .config
-sed -i 's/CONFIG_PACKAGE_luci-app-vsftpd/#CONFIG_PACKAGE_luci-app-vsftpd/g' .config
-sed -i 's/CONFIG_PACKAGE_luci-app-filetransfer/#CONFIG_PACKAGE_luci-app-filetransfer/g' .config
-sed -i 's/CONFIG_PACKAGE_luci-app-vlmcsd/#CONFIG_PACKAGE_luci-app-vlmcsd/g' .config
-
-
 ## 添加5.10/5.15内核的QCA支持
 #wget -P target/linux/ipq40xx/files/arch/arm/boot/dts/ https://github.com/zxlhhyccc/acc-imq-bbr/raw/master/master/qca/target/linux/ipq40xx/files/arch/arm/boot/dts/qcom-ipq4019-c526a.dts
 #rm -f ./package/firmware/ipq-wifi/Makefile
