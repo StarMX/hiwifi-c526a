7e22868fce09c27ece8b4fdbbd7deb8f74a40b0f
StarMX <5440909@qq.com>
2022-06-02 19:59:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git packages https://github.com/coolsnowwolf/packages'>>feeds.conf.default
 #echo 'src-git luci https://github.com/coolsnowwolf/luci'>>feeds.conf.default
 #echo 'src-git routing https://github.com/coolsnowwolf/routing'>>feeds.conf.default
