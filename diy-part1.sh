7690d8756d0dd51b0bc5df6393f2e6107598cc1b
StarMX <5440909@qq.com>
2022-06-04 08:19:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,6 @@ echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.defau
 #echo 'src-git luci https://github.com/coolsnowwolf/luci'>>feeds.conf.default
 #echo 'src-git routing https://github.com/coolsnowwolf/routing'>>feeds.conf.default
 #echo 'src-git telephony https://git.openwrt.org/feed/telephony.git'>>feeds.conf.default
+
+#更换内核 5.4  5.10 5.15
+sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/ramips/Makefile
